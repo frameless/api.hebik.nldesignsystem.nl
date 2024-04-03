fed4ebaf0df90a63954ddc43b9d44dc37f07c8bc
ryanvvalkenhoef <76593083+ryanvvalkenhoef@users.noreply.github.com>
2024-04-03 18:12:41 +0200

    Setup REST-api for fetching crawlData

---
+++ server.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import fs from 'fs';
+
+let data = fs.readFileSync('crawlData.json')
+let parsedData = JSON.parse(data);
+
+const app = express();
+
+app.listen(process.env.PORT || 3000, () => listen);
+
+function listen() {
+    const host = server.address().address;
+    const port = server.address().port;
+    console.log('Example app listening at http://' + host + ':' + port);
+}
+
+app.use(express.static('public'));
+
+let users;
+if (fs.existsSync('crawlData.json')) {
+    console.log('Loading crawldata');
+    let txt = fs.readFileSync('crawlData.json', 'utf8');
+    users = JSON.parse(txt);
+} else {
+    console.log('No crawldata found');
+    users = {};
+}
+
+app.get('/all', showAll);
+app.get('/add/:userid/:crawledobject', addEntry);
+
+function addEntry(req, res) {
+    let userid = req.params.userid;
+
+    let crawledObject = req.params.crawledObject;
+
+    users[userid] = crawledObject;
+
+    let reply = {
+        status: 'success',
+        userid: userid,
+        crawledObject: crawledObject
+    }
+    console.log('Adding: ' + JSON.stringify(reply));
+
+    let json = JSON.stringify(users, null, 2);
+    fs.writeFile('crawlData.json', json, 'utf8', finished);
+    function finished(err) {
+        console.log('Finished writing crawlData.json');
+        res.send(reply);
+    }
+}
+
+function showAll(req, res) {
+    res.send(data);
+}
